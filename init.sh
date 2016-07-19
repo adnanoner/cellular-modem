9126cf195e02dfcfe88d697c948c4d485f2981ce
Adnan Oner <adnanoner@gmail.com>
2016-07-19 16:03:33 +0200

    Switch Huawei modem mode

--- init.sh
+++ init.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+/usr/bin/sg_raw /dev/sr0 11 06 20 00 00 00 00 00 01 00
+
+ifconfig eth1 192.168.123.100
 
 # Huawei
-usb_modeswitch -v 12d1 -p 14dc --verbose -J
+usb_modeswitch -v 12d1 -p 1f01 -M '55534243123456780000000000000011062000000100000000000000000000' --verbose -J
 
 # ZTE
 # usb_modeswitch -v 19d2 -p fff5 --verbose -K
